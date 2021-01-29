1714cf1cc1311bbd06a9adc26289167b2f5570c6
ShirshakKandel <shirshakkandel@gmail.com>
2021-01-29 14:03:10 +0545

    5)Delete functionality added

--- src/Chat.js
+++ src/Chat.js
@@ -6,11 +6,16 @@ import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import db from "./Firebase";
 import ChatInput from "./ChatInput";
+import { useStateValue } from "./StateProvider";
+
 
 function Chat({ toggle }) {
    const { roomId } = useParams(); //useParams is React hook
    const [roomDetails,setRoomDetails] = useState(null);
    const [roomMessages,setRoomMessages] = useState([]);
+   const [{ user },dispatch] = useStateValue();
+   const displayName = user.displayName;
+
 
    useEffect(() => {
       if (roomId) {
@@ -25,14 +30,23 @@ function Chat({ toggle }) {
          .collection("messages")
          .orderBy("timestamp","asc")
          .onSnapshot((snapshot) =>
-            setRoomMessages(snapshot.docs.map((doc) => doc.data()))
-         );
+            setRoomMessages(snapshot.docs.map((doc) => ({
+               id: doc.id,
+               data: doc.data(),
+               displayName: displayName,
+               roomId: roomId
+            })
+            )));
    },[roomId]);
    console.log(roomDetails);
    console.log("Message >>>>",roomMessages);
 
    return (
-      <div className={`chat  ${toggle && "library-active"}`} >
+
+      < div className={`chat  ${toggle && "library-active"}`
+      } >
+         {console.log("User displayname",displayName)}
+
          <div className="chat__header">
             <div className="chat__headerLeft">
                <h4 className="chat__channelName">
@@ -48,12 +62,15 @@ function Chat({ toggle }) {
          </div>
 
          <div className="chat__message">
-            {roomMessages.map(({ message,timestamp,user,userImage }) => (
+            {roomMessages.map(({ data: { message,timestamp,user,userImage },id,displayName,roomId }) => (
                <Message
                   message={message}
                   timestamp={timestamp}
                   user={user}
                   userImage={userImage}
+                  displayName={displayName}
+                  id={id}
+                  roomId={roomId}
                />
             ))}
          </div>
--- src/Message.js
+++ src/Message.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Message.css";
+import DeleteIcon from '@material-ui/icons/Delete';
+import db from "./Firebase";
 
-function Message({ message, timestamp, user, userImage }) {
+function Message({ message,timestamp,user,userImage,displayName,id,roomId }) {
+   const deleteMessage = (id,roomId) => {
+      db.collection("rooms/roomId/message/id").delete()
+
+   }
    return (
       <div className="message">
          <img src={userImage} alt="" />
          <div className="message__info">
             <h4>
-               {user}{" "}
+               {user}
                <span className="message__timestamp">
-                  {new Date(timestamp?.toDate()).toLocaleString().split(",")[0]}
+                  {new Date(timestamp?.toDate()).toLocaleString().split(",")[0]
+
+                  }
+                  {displayName === user && <DeleteIcon onClick={e => db.collection("rooms").doc(roomId).collection("messages").doc(id).delete()} />}
+
                </span>
             </h4>
             <p>{message}</p>
